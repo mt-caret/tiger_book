19401ef691b0b510f79aeaeb610d8b83e4b3b9dc
Masayuki Takeda <mtakeda.enigsol@gmail.com>
2018-10-23 21:34:58 +0900

    add mly

---
+++ ch2/realworldocaml/json.ml
@@ -0,0 +1,11 @@
+open Core_kernel
+
+type t = [
+  | `Assoc of (string * t) list
+  | `Bool of bool
+  | `Float of float
+  | `Int of int
+  | `List of t list
+  | `Null
+  | `String of string
+  ] [@@deriving sexp]
