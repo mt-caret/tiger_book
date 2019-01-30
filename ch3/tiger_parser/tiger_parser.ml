44822f8b49ca1945d77d23447c553236fce166c3
Masayuki Takeda <mtakeda.enigsol@gmail.com>
2019-01-30 02:26:19 +0000

    remove unnecessary code

--- ch3/tiger_parser/tiger_parser.ml
+++ ch3/tiger_parser/tiger_parser.ml
@@ -1,35 +1,6 @@
 open Core
 open Async
 
-(*
-let attempt_lex (lexbuf : Lexing.lexbuf) =
-  Result.try_with (fun () -> Lexer.read lexbuf)
-  |> Result.map_error ~f:(function
-      | Lexer.SyntaxError msg ->
-        let pos = lexbuf.lex_curr_p in
-        sprintf "%s (line %d column %d): %s"
-          pos.pos_fname
-          pos.pos_lnum
-          (pos.pos_cnum - pos.pos_bol + 1)
-          msg
-      | x ->
-        Exn.to_string x
-    )
-;;
-
-let run_lexer ~filename =
-  let%map source_code = Reader.with_file filename ~f:Reader.contents in
-  let lexbuf = Lexing.from_string source_code in
-  let rec lex accum =
-    let open Result.Let_syntax in
-    match%bind attempt_lex lexbuf with
-    | Parser.EOF -> Ok (List.rev (Parser.EOF :: accum))
-    | token -> lex (token :: accum)
-  in
-  lex []
-;;
-*)
-
 let create_error_string (lexbuf : Lexing.lexbuf) error_message =
   let pos = lexbuf.lex_curr_p in
   sprintf "%s (line %d column %d): %s"
@@ -47,7 +18,7 @@ let attempt_parse (lexbuf : Lexing.lexbuf) =
       | Parser.Error ->
         create_error_string lexbuf "syntax error"
       | x ->
-        Exn.to_string x
+        create_error_string lexbuf (Exn.to_string x)
     )
 ;;
 
